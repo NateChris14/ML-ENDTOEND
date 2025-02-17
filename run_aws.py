18292c350f5360be5edd64158a8eca9a6146638c
Nathan Christopher Menon <menonnathanchristopher@gmail.com>
2025-02-17 17:38:14 +0000

    aws run

--- run_aws.py
+++ run_aws.py
@@ -1,12 +1,14 @@
 import subprocess
+
 command = [
     "aws", "codebuild", "create-project",
     "--name", "my-app-build",
     "--source", "type=GITHUB,location=https://github.com/NateChris14/ML-ENDTOEND",
     "--artifacts", "type=NO_ARTIFACTS",
-    "--environment", "type=LINUX_CONTAINER,image=aws/codebuild/python:3.9,computeType=BUILD_GENERAL1_SMALL",
+    "--environment", "type=LINUX_CONTAINER,image=aws/codebuild/python:3.12,computeType=BUILD_GENERAL1_MEDIUM",  # Updated to Python 3.12 and medium compute
     "--service-role", "arn:aws:iam::145023104239:role/CodeBuildServiceRole",
     "--timeout-in-minutes", "30",
     "--tags", "key=Project,value=FlaskApp"
 ]
+
 subprocess.run(command)
